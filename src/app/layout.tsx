47ff9bc83a38297c3eda68c05ce5629d3d97425e
LEONARDO <leosivi@gmail.com>
2026-01-29 09:34:28 -0400

    Ajuste no icone da navbar e no favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/icone-de-cao-e-gato.avif",
+  },
+};
 
 export default function RootLayout({
   children,
